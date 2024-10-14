daf31f71307b5984d332ef77fcc06203375f31ee
seulki <tmfrl4710@gmail.com>
2024-10-14 15:33:16 +0900

    feat: JsonUtils 생성

---
+++ core/src/main/java/com/taskbuddy/core/utils/JsonUtils.java
@@ -0,0 +1,21 @@
+package com.taskbuddy.core.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonUtils {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_MAPPER.registerModule(new JavaTimeModule());
+    }
+
+    public static String serialize(Object target) {
+        try {
+            return OBJECT_MAPPER.writeValueAsString(target);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("failed to serialize : " + target.toString(), e);
+        }
+    }
+}
