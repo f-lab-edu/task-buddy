f6b8e8e824e573c6254c297540fcf028ce8c5fa4
seulki <tmfrl4710@gmail.com>
2024-11-04 19:22:46 +0900

    refactor: core 모듈 제거

--- core/src/main/java/com/taskbuddy/core/database/repository/TaskReminderJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskbuddy.core.database.repository;
-
-import com.taskbuddy.persistence.entity.TaskReminderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskReminderJpaRepository extends JpaRepository<TaskReminderEntity, Long> {
-}
