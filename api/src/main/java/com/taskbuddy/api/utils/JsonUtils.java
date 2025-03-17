aff8fc776754945896269db41618333e18c739f7
olivejua <tmfrl4710@gmail.com>
2025-03-17 23:15:52 +0900

    style: JsonUtils 메서드 javadoc 추가

--- api/src/main/java/com/taskbuddy/api/utils/JsonUtils.java
+++ api/src/main/java/com/taskbuddy/api/utils/JsonUtils.java
@@ -11,6 +11,13 @@ public class JsonUtils {
         OBJECT_MAPPER.registerModule(new JavaTimeModule());
     }
 
+    /**
+     * 객체를 JSON 문자열로 직렬화한다.
+     *
+     * @param target 직렬화할 객체
+     * @return JSON 문자열
+     * @throws RuntimeException JSON 변환 중 오류가 발생한 경우 예외를 던집니다.
+     */
     public static String serialize(Object target) {
         try {
             return OBJECT_MAPPER.writeValueAsString(target);
@@ -19,6 +26,15 @@ public class JsonUtils {
         }
     }
 
+    /**
+     * JSON 문자열을 주어진 타입의 객체로 역직렬화한다.
+     *
+     * @param target JSON 문자열
+     * @param returnClass 변환할 객체 타입
+     * @param <R> 변환할 객체의 타입
+     * @return 역직렬화된 객체
+     * @throws RuntimeException JSON 변환 중 오류가 발생한 경우 예외를 던진다.
+     */
     public static <R> R deserialize(String target, Class<R> returnClass) {
         try {
             return OBJECT_MAPPER.readValue(target, returnClass);
